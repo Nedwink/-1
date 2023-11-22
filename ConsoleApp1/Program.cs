87bcaa82c52c084c7eab987dd29160f49ed4f197
Nedwink <Nedwink@DESKTOP-B51HIF7>
2023-11-22 15:37:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                            (Задание 9)
+            string number1String = Microsoft.VisualBasic.Interaction.InputBox("Введите первое число:", "Число 1", "");
+            string number2String = Microsoft.VisualBasic.Interaction.InputBox("Введите второе число:", "Число 2", "");
+            double number1 = Convert.ToDouble(number1String);
+            double number2 = Convert.ToDouble(number2String);
+            double sum = number1 + number2;
+            MessageBox.Show("Сумма: " + sum, "Результат");
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+
+}
+   
---
+++ С# 1/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+
+namespace С__1
+{
+    class Lesson
+    {
+        static void Main(string[] args)
+        {
+            (Задание 2)
+            Console.WriteLine("Введите имя: ");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Введите Фамилию");
+            string secondname = Console.ReadLine();
+            Console.WriteLine($"Ваше имя : {firstname}");
+            Console.WriteLine($"Ваша Фамилия : {secondname}");
+            Console.ReadLine();
+            Console.ReadKey();
+
+            (Задание 3)
+            Console.WriteLine();
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите ваш возраст: "); int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваше имя: " + name);
+            Console.WriteLine("Ваш возраст: " + age);
+            Console.Title = "Ваше имя и возраст";
+            string txt = "Ваше имя: " + name + " Ваш возраст:  " + age;
+            Console.WriteLine(txt);
+            Console.ReadKey();
+
+            (Задание 4)
+            Console.WriteLine("Название текушего дня недели: ");
+            string dayofweek = Console.ReadLine();
+            Console.WriteLine("Введите название месяца: ");
+            string month = Console.ReadLine();
+            Console.WriteLine("Введите номер месяца: ");
+            string dayofmonth = Console.ReadLine();
+            Console.WriteLine($"Текущий день недели : {dayofmonth}");
+            Console.WriteLine($"Название месяца : {month}");
+            Console.WriteLine($"Номер дня в месяце: {dayofmonth}");
+            Console.ReadLine();
+            Console.ReadKey();
+
+            (Задание 5)
+            Console.Write("Введите ваш год рождения: ");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+            int currentYear = DateTime.Now.Year;
+            int age = currentYear - yearOfBirth;
+            Console.WriteLine("Ваш возраст: " + age);
+            Console.ReadLine();
+
+            (Задание 6)
+            Console.Write("Введите ваш возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            int currentYear = DateTime.Now.Year;
+            int birthYear = currentYear - age;
+            Console.WriteLine("Ваш год рождения: " + birthYear);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+            (Задание 8)
+            Console.Write("Введите первое число: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            double sum = number1 + number2;
+            Console.WriteLine("Сумма: " + sum);
+            Console.ReadLine();
+
+            (Задание 10)
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Последовательность: " + (number - 1) + ", " + number + ", " + (number + 1));
+            Console.ReadLine();
+
+
+
+            (Задание 11)
+            Console.Write("Введите первое число: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            double sum = number1 + number2;
+            double difference = number1 - number2;
+            Console.WriteLine("Сумма: " + sum);
+            Console.WriteLine("Разность: " + difference);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
